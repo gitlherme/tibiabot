5441ca7f14869c20d30152f15e77f2315f92e30e
Guilherme Vieira <dev@guilhermevieira.dev>
2022-06-23 13:27:11 -0300

    fix: adjusts in verifyMember

--- src/commands/verifyMember.js
+++ src/commands/verifyMember.js
@@ -2,16 +2,19 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 const { MessageEmbed } = require('discord.js');
 const getCharacterData = require('../utils/getCharacterInfo')
 
-function generateResponse(type, data) {
+function generateResponse(type) {
 	const description = {
 		success: `Tudo certo! Verifique se possui o acesso completo ao Discord. 🥳`,
-		error: `Ops! Parece que esse personagem não existe ou não faz parte da Guild. 😵`
+		error: `Ops! Parece que esse personagem não existe ou não faz parte da Guild. 😵`,
+		verified: `O seu personagem já foi verificado antes. Você já possui acesso completo ao Discord. 😁`
 	}
 
 	const embed = new MessageEmbed()
 		.setTitle('Verificação do personagem')
 		.setDescription(description[type])
 		.setFooter({ text: 'Criado por Vieirito Shinigami (twitter.com/gitlherme)' })
+	
+	return { embeds: [embed] }
 }
 
 module.exports = {
@@ -22,9 +25,11 @@ module.exports = {
 			.setDescription('Nome do personagem pra registro')
 			.setRequired(true)),
 	async execute(interaction) {
-    const character = await getCharacterData()
-		console.log(character)
-		if (character.guild.name === 'Harty Punt') await interaction.reply(await generateResponse('success'));
+		const characterToBeVerified = interaction.options.getString('name')
+    const character = await getCharacterData(characterToBeVerified)
+		if (character.guild.name === 'Harty Punt') {
+			await interaction.reply(await generateResponse('success'));
+		}
 		if (character.guild.name !== 'Harty Punt') await interaction.reply(await generateResponse('error'));
 	},
 };
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 require('dotenv').config()
+const { Client, Intents, Collection } = require('discord.js')
 const fs = require('node:fs');
 const path = require('node:path');
-const { Client, Intents, Collection } = require('discord.js')
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
 const token = process.env.BOT_TOKEN
 const clientId = process.env.CLIENT_ID 
+
 const client = new Client({
   intents: [
     Intents.FLAGS.GUILDS,
@@ -13,37 +14,30 @@ const client = new Client({
   ],
 });
 
-const commands = [];
+const commands = []
 client.commands = new Collection();
 const commandsPath = path.join(__dirname, 'commands');
 const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
+	const filePath = path.join(commandsPath, file);
+	const command = require(filePath);
 	commands.push(command.data.toJSON());
+	client.commands.set(command.data.name, command)
 }
+
 const rest = new REST({ version: '9' }).setToken(token);
 
-(async () => {
-	try {
-		console.log('Started refreshing application (/) commands.');
-
-		await rest.put(
-			Routes.applicationCommands(clientId),
-			{ body: commands },
-		);
-
-		console.log('Successfully reloaded application (/) commands.');
-	} catch (error) {
-		console.error(error);
-	}
-})();
+rest.put(Routes.applicationCommands(clientId), { body: commands })
+	.then(() => console.log('Successfully registered application commands.'))
+	.catch(console.error);
 
 client.on('interactionCreate', async interaction => {
 	if (!interaction.isCommand()) return;
+
 	const command = client.commands.get(interaction.commandName);
-	console.log(command)
 	if (!command) return;
+
 	try {
 		await command.execute(interaction);
 	} catch (error) {
