9173b0670d7895f7c9515a1383c1f6d3ee77dc10
= <=>
2022-06-23 14:32:58 -0300

    cfeat(verify-member): create giveAccess and changeName functions

--- src/commands/verifyMember.js
+++ src/commands/verifyMember.js
@@ -1,6 +1,23 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
-const { MessageEmbed } = require('discord.js');
+const { MessageEmbed, Guild } = require('discord.js');
 const getCharacterData = require('../utils/getCharacterInfo')
+
+
+async function giveAccess(client, interaction) {
+	const server = client.guilds.cache.get(interaction.guildId)
+	const member = await server.members.fetch(interaction.user.id)
+	const role = await server.roles.fetch('989562433666449479')
+	member.roles.add(role)
+}
+
+async function changeName(client, interaction, name, vocation) {
+	const server = client.guilds.cache.get(interaction.guildId)
+	const member = await server.members.fetch(interaction.user.id)
+	if (vocation.includes('Knight')) member.setNickname(`[EK] ${name}`)
+	if (vocation.includes('Paladin')) member.setNickname(`[RP] ${name}`)
+	if (vocation.includes('Druid')) member.setNickname(`[ED] ${name}`)
+	if (vocation.includes('Sorcerer')) member.setNickname(`[MS] ${name}`)
+}
 
 function generateResponse(type) {
 	const description = {
@@ -24,10 +41,12 @@ module.exports = {
 		.addStringOption(option => option.setName('name')
 			.setDescription('Nome do personagem pra registro')
 			.setRequired(true)),
-	async execute(interaction) {
+	async execute(interaction, client) {
 		const characterToBeVerified = interaction.options.getString('name')
     const character = await getCharacterData(characterToBeVerified)
 		if (character.guild.name === 'Harty Punt') {
+			giveAccess(client, interaction)
+			changeName(client, interaction, character.name, character.vocation)
 			await interaction.reply(await generateResponse('success'));
 		}
 		if (character.guild.name !== 'Harty Punt') await interaction.reply(await generateResponse('error'));
